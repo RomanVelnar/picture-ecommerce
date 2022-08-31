583fec333c0ee067d812f450bb032629659abe6a
Roman Velnar <roman.velnar@gmail.com>
2022-08-31 14:50:38 +0100

    worked on the favorite logic

--- src/Context.js
+++ src/Context.js
@@ -13,10 +13,22 @@ function ContextProvider({children}) {
             .then(data => setAllPhotos(data))
     }, [])
 
-    console.log(setAllPhotos)
+    function toggleFavorite(id) {
+        const updateArr = allPhotos.map(photo => {
+            if(photo.id === id) {
+                console.log(id)
+                console.log(!photo.isFavorite)
+
+                return {...photo, isFavorite: !photo.isFavorite}
+            }
+            return photo
+        })
+
+        setAllPhotos(updateArr);
+    }
 
     return(
-        <Context.Provider value={{allPhotos}}>
+        <Context.Provider value={{allPhotos, toggleFavorite}}>
             {children}
         </Context.Provider>
     )
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "../Context"
 
 function Image({className, img}) {
+    const [hovered, setHovered] = useState(false);
+    const {toggleFavorite} = useContext(Context);
+
+    function heartIcon() {
+        if(img.isFavorite) {
+            return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
+        } else if(hovered) {
+            return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+        }
+    }
+
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
     return(
-        <div className={`${className} image-container`}>
+        <div 
+            className={`${className} image-container`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
             <img src={img.url} className="image-grid"/>
+            {heartIcon()}
+            {cartIcon}
         </div>
     )
 }
