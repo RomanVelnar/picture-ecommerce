eb02bbb9facc15b6a348810549c04bfbab1ad896
Roman Velnar <roman.velnar@gmail.com>
2022-08-31 15:53:37 +0100

    added the cart logo in the right corner

--- src/Context.js
+++ src/Context.js
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react";
 const Context = React.createContext()
 
 function ContextProvider({children}) {
-    const [allPhotos, setAllPhotos] = useState([])
+    const [allPhotos, setAllPhotos] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
 
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
 
@@ -16,9 +17,6 @@ function ContextProvider({children}) {
     function toggleFavorite(id) {
         const updateArr = allPhotos.map(photo => {
             if(photo.id === id) {
-                console.log(id)
-                console.log(!photo.isFavorite)
-
                 return {...photo, isFavorite: !photo.isFavorite}
             }
             return photo
@@ -27,8 +25,16 @@ function ContextProvider({children}) {
         setAllPhotos(updateArr);
     }
 
+    function addToCart(newItem) {
+        setCartItems(prevItems => [...prevItems, newItem])
+    }
+
+    function removeFromCart(id) {
+        setCartItems(prevItems => prevItems.filter(item => item.id !== id))
+    }
+
     return(
-        <Context.Provider value={{allPhotos, toggleFavorite}}>
+        <Context.Provider value={{allPhotos, toggleFavorite, addToCart, cartItems, removeFromCart}}>
             {children}
         </Context.Provider>
     )
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,10 @@
 import React, { useState, useContext } from "react";
-import { Context } from "../Context"
+import PropTypes from "prop-types";
+import { Context } from "../Context";
 
 function Image({className, img}) {
     const [hovered, setHovered] = useState(false);
-    const {toggleFavorite} = useContext(Context);
+    const {toggleFavorite, addToCart, cartItems, removeFromCart} = useContext(Context);
 
     function heartIcon() {
         if(img.isFavorite) {
@@ -13,7 +14,14 @@ function Image({className, img}) {
         }
     }
 
-    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+    function cartIcon() {
+        const alreadyInCart = cartItems.some(item => item.id === img.id)
+        if(alreadyInCart) {
+            return <i className="ri-shopping-cart-fill cart" onClick={() => removeFromCart(img.id)}></i>
+        } else if(hovered) {
+            return <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
+        }
+    }
 
     return(
         <div 
@@ -23,9 +31,18 @@ function Image({className, img}) {
         >
             <img src={img.url} className="image-grid"/>
             {heartIcon()}
-            {cartIcon}
+            {cartIcon()}
         </div>
     )
+}
+
+Image.propTypes = {
+    className: PropTypes.string,
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    })
 }
 
 export default Image;
