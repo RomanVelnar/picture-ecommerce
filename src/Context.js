90128d767ee7a08525687227cb86a9358d3b076f
Roman Velnar <roman.velnar@gmail.com>
2022-08-31 09:01:23 +0100

    fetched the API data

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
     const [allPhotos, setAllPhotos] = useState([])
+
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
+    }, [])
 
     return(
         <Context.Provider value={{allPhotos}}>
