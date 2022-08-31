60523855bb5e380bc16ed537513cbd0397d61f9a
Roman Velnar <roman.velnar@gmail.com>
2022-08-31 17:26:50 +0100

    added some minor UX improvements

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -26,9 +26,13 @@ function Cart() {
             <h1>Check out</h1>
             {cartItemElements}
             <p className="total-cost">Total: {totalCostDisplay}</p>
-            <div className="order-button">
-                <button onClick={placeOrder}>{buttonText}</button>
-            </div>
+            {
+                cartItems.length > 0 ?   
+                <div className="order-button">
+                    <button onClick={placeOrder}>{buttonText}</button>
+                </div> :
+                <p>there are no items in the cart.</p>
+            }
         </main>
     )
 }
